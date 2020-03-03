c6329957899a7e573015f719e3f14f5161cc2331
Shubha Mittal <shubha.mittal26@gmail.com>
2020-03-03 11:09:19 -0800

    Second Commit

---
+++ images/store.js
@@ -0,0 +1,68 @@
+const STORE = {
+    questions:[
+       
+        {
+           question:"Which country has the largest population in Africa?",
+           options:[
+              "Egypt",
+              "South Africa",
+              "Ethiopia",
+              "Nigeria"
+           ],
+           correct:"Nigeria"
+        },
+
+        {
+            question:"What is Earth's largest continent by surface size?",
+            options:[
+                "Asia",
+                "Africa",
+                "North America",
+                "Europe"
+            ],
+            correct:"Asia"
+
+        },
+
+        {
+            question:"What is the most widely spoken language of the world (by first language speakers)?",
+            options:[
+                "English",
+                "Hindi",
+                "Mandarin",
+                "Spanish"
+            ],
+            correct: "Mandarin"
+        },
+
+        {
+            question:"What is by area smallest independent country on earth?",
+            options:[
+                "Nauru",
+                "San Marino",
+                "Morocco",
+                "Vatican City"
+            ],
+            correct:"Vatican City"
+        },
+
+        {
+            question:"Which of these is largest river by discahrge of water in the world?",
+            options:[
+                "Nile",
+                "Mississippi",
+                "Mekong",
+                "Amazon"
+            ],
+            correct:"Amazon"
+        }
+    
+    ]
+}
+
+
+
+
+
+
+
